d0fee6338a41ff7645b44c9770c1d405d2089301
Mohamed Adam Ata <115686926+CODINATA@users.noreply.github.com>
2025-04-04 21:34:29 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyChjZjiiGfbTnJhTXy4TuCYZzdkR8l36do",
-    authDomain: "agency-website-77f20.firebaseapp.com",
-    projectId: "agency-website-77f20",
-    storageBucket: "agency-website-77f20.firebasestorage.app",
-    messagingSenderId: "812034971816",
-    appId: "1:812034971816:web:3f810e41104409415c4a9d",
-    measurementId: "G-GZL7SMC1HF"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
